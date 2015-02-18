9214bf81b2530e4114bdcc44d8c4ea5312829572
Blair Anderson <blair@repairshopr.com>
2015-02-18 00:02:15 -0800

    swagger gh-pages

--- app/models/item.rb
+++
@@ -1,5 +0,0 @@
-class Item < ActiveRecord::Base
-  belongs_to :user
-  has_many :votes, as: :votable
-  has_many :comments, class_name: "ItemComment"
-end
--- app/models/item_comment.rb
+++
@@ -1,5 +0,0 @@
-class ItemComment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :item, counter_cache: :comments_count
-  validates_presence_of :user_id, :item_id, :content
-end
