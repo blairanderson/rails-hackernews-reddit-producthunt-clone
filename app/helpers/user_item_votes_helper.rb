0e50272e06ede47a855b093638a954c16a5c42f2
Blair Anderson <blair@repairshopr.com>
2015-03-28 14:43:04 -0700

    update theme

--- app/helpers/user_item_votes_helper.rb
+++ app/helpers/user_item_votes_helper.rb
@@ -7,8 +7,8 @@ module UserItemVotesHelper
     link_to 'unlike!', vote_item_path(object), method: :delete, class: 'text-muted'
   end
 
-  def render_votes_for_item(item)
-    if @votes[item.id].include?(current_user.id)
+  def render_votes_for_item(item=nil)
+    if current_user && @votes[item.id].include?(current_user.id)
       link_to_downvote(item)
     else
       link_to_upvote(item)
